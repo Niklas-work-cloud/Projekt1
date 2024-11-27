af290aab26182d7e3a32c29782b72107b545c4f2
Niklas ROTHGANG <niklas.rothgang@valeo.com>
2024-11-27 08:46:30 +0100

    2

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
+# main.py
 import streamlit as st
 from streamlit_option_menu import option_menu
-import os
-from dotenv import load_dotenv
-from streamlit_option_menu import option_menu
-
-# Umgebungsvariablen laden
-load_dotenv()
-
-# Importiere deine App-Module
 import home
 import diagramme
 import zeitschaltplan
 import wachstumsfortschritt
 import historische_daten
+from dotenv import load_dotenv
 
-# Streamlit Seite konfigurieren
+# Umgebungsvariablen laden
+load_dotenv()
+
+# Streamlit-Seite konfigurieren
 st.set_page_config(
     page_title="Pondering",
 )
@@ -25,24 +22,19 @@ class MultiApp:
     def __init__(self):
         self.apps = []
 
-    # Funktion zum Hinzufügen von Apps
     def add_app(self, title, func):
         self.apps.append({
             "title": title,
             "function": func
         })
 
-    # Methode, um die Apps zu starten
     def run(self):
-        # Sidebar für Navigation
         with st.sidebar:
             st.sidebar.image("images/Grow_smart.png")
-
             st.markdown("---")
 
-            # Sidebar für Navigation
             app = option_menu(
-                menu_title="🌱 Gardening Box ",
+                menu_title="🌱 Gardening Box",
                 options=["Home", "Diagramme", "Zeitschaltplan", "Wachstumsfortschritt", "Historische Daten"],
                 icons=["house-fill", "📊", "⏱️", "🌱", "clock"],
                 menu_icon="🌱",
@@ -75,15 +67,12 @@ class MultiApp:
 
 # Hauptprogramm
 if __name__ == "__main__":
-    # MultiApp-Instanz erstellen und Apps hinzufügen
     app = MultiApp()
 
-    # Hier kannst du Apps hinzufügen
     app.add_app("Home", home.app)
     app.add_app("Diagramme", diagramme.app)
     app.add_app("Zeitschaltplan", zeitschaltplan.app)
     app.add_app("Wachstumsfortschritt", wachstumsfortschritt.app)
     app.add_app("Historische Daten", historische_daten.app)
     
-    # Die Apps ausführen
     app.run()
